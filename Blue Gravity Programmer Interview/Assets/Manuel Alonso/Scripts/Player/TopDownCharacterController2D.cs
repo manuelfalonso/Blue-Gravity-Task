d7fa1ae4332a2a47be9e7b5fe14bd87ecd7134f5
manuelfalonso <manuelalonso89@gmail.com>
2023-04-15 19:39:46 -0300

    Added and integrated package Mighty Heroes (Rogue) 2D Fantasy Characters Pack

--- Blue Gravity Programmer Interview/Assets/Manuel Alonso/Scripts/Player/TopDownCharacterController2D.cs
+++ Blue Gravity Programmer Interview/Assets/Manuel Alonso/Scripts/Player/TopDownCharacterController2D.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,11 +7,14 @@ public class TopDownCharacterController2D : MonoBehaviour
     [SerializeField]
     private PlayerInput _playerInput = default(PlayerInput);
     [SerializeField]
-    private Rigidbody2D _rigidbody;
+    private Rigidbody2D _rigidbody = default;
     [SerializeField]
     private float _movementSpeed = 1f;
+    [SerializeField]
+    private Flip _flipBehaviour = default(Flip);
 
-    private Vector2 moveAmount = new Vector2();
+    private Vector2 _moveAmount = new Vector2();
+    private bool _flipX = false;
 
 
     public void Update()
@@ -22,7 +26,8 @@ public class TopDownCharacterController2D : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         // read the value for the "move" action each event call
-        moveAmount = context.ReadValue<Vector2>();
+        _moveAmount = context.ReadValue<Vector2>();
+        CheckFlip(_moveAmount);
     }
 
 
@@ -30,7 +35,33 @@ public class TopDownCharacterController2D : MonoBehaviour
     {
         // to use the Vector2 value from the "move" action each
         // frame, use the "moveAmount" variable here.
-        var movement = _rigidbody.position + moveAmount * _movementSpeed;
+        var movement = _rigidbody.position + _moveAmount * _movementSpeed;
         _rigidbody.MovePosition(movement);
+    }
+
+    private void CheckFlip(Vector2 movement)
+    {
+        if (movement.x > 0)
+        {
+            if (_flipX)
+            {
+                Flip();
+                _flipX = false;
+            }
+        }
+        else if (movement.x < 0)
+        {
+            if (!_flipX)
+            {
+                Flip();
+                _flipX = true;
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        transform.localScale =
+            new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
