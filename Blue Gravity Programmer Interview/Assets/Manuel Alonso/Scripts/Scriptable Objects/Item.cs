ef8b53fec241ea8495c0ffd9be3fe05f4ac20d13
manuelfalonso <manuelalonso89@gmail.com>
2023-04-16 12:34:11 -0300

    Added InteractionHandler.cs to Shopkeeper

--- Blue Gravity Programmer Interview/Assets/Manuel Alonso/Scripts/Scriptable Objects/Item.cs
+++ Blue Gravity Programmer Interview/Assets/Manuel Alonso/Scripts/Scriptable Objects/Item.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     public int Id = 0;
+    public string Name = string.Empty;
 }
---
+++ Blue Gravity Programmer Interview/Assets/Manuel Alonso/Scripts/Shop/ShopController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void LoadShop()
+    {
+
+    }
+
+    public void Buy()
+    {
+
+    }
+
+    public void Sell()
+    {
+
+    }
+}
