7649b52d84dbb37c148f771fbd0c473900e8f5e4
manuelfalonso <manuelalonso89@gmail.com>
2023-04-18 19:56:04 -0300

    Added Tapestry Event system

---
+++ Blue Gravity Programmer Interview/Assets/Manuel Alonso/Scripts/Utils/TapestryEventRegistry.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script was imported
+namespace Tapestry
+{
+    public class TapestryEventRegistry
+    {
+
+        #region Example
+        /// <summary>
+        /// Reports when Map objects are instantiated
+        /// </summary>
+        public static TapestryEvent<GameObject> OnObjectSpawnedExample;
+        #endregion
+
+        static TapestryEventRegistry()
+        {
+            CreateTapestryEvents();
+        }
+
+        private static void CreateTapestryEvents()
+        {
+
+            #region Example
+            OnObjectSpawnedExample = new TapestryEvent<GameObject>();
+            #endregion
+        }
+
+        public static void OnDestroy()
+        {
+            // Creates new events to clear the old ones
+            CreateTapestryEvents();
+        }
+    }
+}
